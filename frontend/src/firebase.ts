fb0e81500d2ffcc85ec5d7a3cf7a36fef2742e69
MailBIT Sales <yuvalleberstein21@gmail.com>
2025-07-24 11:26:58 +0300

    .env file

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,17 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAyXtrXmRvg1Iyq4KZi05gjYX9J27NjStE',
-  authDomain: 'babymemories-5764d.firebaseapp.com',
-  projectId: 'babymemories-5764d',
-  storageBucket: 'babymemories-5764d.firebasestorage.app',
-  messagingSenderId: '268985935425',
-  appId: '1:268985935425:web:3239d143e11fc4a8da48c0',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
